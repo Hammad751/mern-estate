37e75bbc781bdb29143377544e1a7977c622e0c8
virtuByVirtual <hammadvtech@gamil.com>
2023-12-09 15:01:42 +0500

    add pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import Profile from './pages/Profile'
 
 export default function App() {
   return (
-    <h1>App</h1>
+    <BrowserRouter className="text-red-500">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signout' element={<SignOut />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
