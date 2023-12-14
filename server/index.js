b36f46cc6e6a0f3b8d6ac01eff4c48b246c70eec
virtuByVirtual <hammadvtech@gamil.com>
2023-12-14 18:34:25 +0500

    create middleware and function for possible error.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.Mongo).then(() =>{
@@ -9,14 +10,24 @@ mongoose.connect(process.env.Mongo).then(() =>{
 }).catch((error) =>{
     console.log(error);
 });
+
 const app = express();
 const port = 8080;
 
+app.use(express.json())
 app.listen(port, () =>{
     console.log(`server running on port http://localhost:${port}`);
 })
 
 app.use('/server/user', userRouter);
-// app.get('/test', (req, res)=>{
-//     res.json({message: "calling api"});
-// })
+app.use('/server/auth', authRouter);
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "internal server error";
+
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+})
