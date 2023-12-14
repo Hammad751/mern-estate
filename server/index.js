9c44bb44092aba802b94b3a1b46336cdf4147ce8
virtuByVirtual <hammadvtech@gamil.com>
2023-12-14 12:55:38 +0500

    create and connect to the database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
 
+mongoose.connect(process.env.Mongo).then(() =>{
+    console.log("DB connected");
+}).catch((error) =>{
+    console.log(error);
+});
 const app = express();
 const port = 8080;
 
