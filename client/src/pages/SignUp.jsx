dc194c44971b67dcb5ffa1400ed0c5b83704b668
virtuByVirtual <hammadvtech@gamil.com>
2023-12-15 12:20:59 +0500

    complete signup page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,28 +1,70 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) =>{
+    setFormData({
+      ...formData, // we have to keep track the changes, so we use spread operator
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // we use this to prevent the page to be reload again and again
+    try {
+      // fetch the data from api and add API request method
+      const res = await fetch('/server/auth/signup', {
+        method:'POST',
+        headers:{
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      // conver the data into string
+      const data = await res.json();
+      // console.log(data);
+      if(data.success == false){
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/signin')
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+    
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
 
-      <form className='flex flex-col gap-4'>
-        <input type='text' placeholder='username' className='border p-3 rounded' id='username' />
-        <input type='email' placeholder='email' className='border p-3 rounded' id='email' />
-        <input type='password' placeholder='password' className='border p-3 rounded' id='password' />
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type='text' placeholder='username' className='border p-3 rounded' id='username' onChange={handleChange}/>
+        <input type='email' placeholder='email' className='border p-3 rounded' id='email' onChange={handleChange} />
+        <input type='password' placeholder='password' className='border p-3 rounded' id='password' onChange={handleChange} />
 
-        <button className='bg-slate-600 text-white 
+        <button disabled={loading} className='bg-slate-600 text-white 
         p-3 rounded
         uppercase hover:opacity-95
         disabled:opacity-80'
-        >Sign Up</button>
-        <div className='flex gap-2'>
+        >{loading ? "loading..." : "Sign Up"}</button>
+      </form>
+      <div className='flex gap-3'>
           <p>Have an account?</p>
           <Link to={"/signin"}>
             <span className='text-blue-600'>Sign in</span>
           </Link>
-        </div>
-      </form>
+      </div>
+
+      {error && <p className='text-red-500'>{error}</p>}
     </div>
   )
 }
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,12 +1,10 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
+// import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req,res, next) =>{
     const { username, email, password } = req.body;
-
     const hashedPass = bcryptjs.hashSync(password, 10);
-    
     const newUser = new User({username, email, password: hashedPass});
 
     try {
